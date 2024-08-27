ae7e51b4e1a54963773401f34bb7003f2aa84791
Youssef Oubrik <youssefoubrik.edu@gmail.com>
2024-08-27 13:45:03 +0100

    Made categories bar more responsive and moved routing logic to parent component

--- app/components/nav/Categories.tsx
+++ app/components/nav/Categories.tsx
@@ -2,22 +2,42 @@
 import { usePathname, useSearchParams } from "next/navigation";
 import { Category } from "./Category";
 import { categories } from "@/app/utils/categories";
+import { useRouter } from "next/navigation";
+import queryString from "query-string";
 
 export const Categories = () => {
   const params = useSearchParams();
   const searchCategory = params?.get("category") || "All";
   const pathname = usePathname();
   const isHome = pathname === "/";
+  const router = useRouter();
   if (!isHome) return null;
+
+  const handleCategoryClick = (label: string) => {
+    if (label === "All") return router.push("/");
+    const url = queryString.stringifyUrl(
+      {
+        url: "/",
+        query: { category: label },
+      },
+      {
+        skipEmptyString: true,
+        skipNull: true,
+      }
+    );
+    router.push(url);
+  };
+
   return (
     <div className="bg-white">
-      <div className="flex items-center justify-center gap-x-3 md:gap-x-14 border-b-[1px] px-2 flex-wrap">
+      <div className="grid grid-cols-3 sm:grid-cols-2 md:grid-cols-7 gap-x-3 md:gap-x-14 border-b-[1px] px-2 md:px-3">
         {categories.map((category) => (
           <Category
             key={category.label}
             label={category.label}
             Icon={category.icon}
             isActive={searchCategory === category.label}
+            onClick={() => handleCategoryClick(category.label)} // Pass the handler function
           />
         ))}
       </div>
--- app/components/nav/Category.tsx
+++ app/components/nav/Category.tsx
@@ -1,54 +1,24 @@
-import { useRouter, useSearchParams } from "next/navigation";
-import queryString from "query-string";
-import { use, useCallback } from "react";
 import { IconType } from "react-icons";
 
-interface INavbar {
+interface ICategory {
   label: string;
   Icon: IconType;
   isActive: boolean;
+  onClick: () => void;
 }
-export const Category: React.FC<INavbar> = ({ label, Icon, isActive }) => {
-  const router = useRouter();
-  //   const params = useSearchParams();
-  const handleClick = useCallback(
-    () => {
-      if (label === "All") return router.push("/");
-      const url = queryString.stringifyUrl(
-        {
-          url: "/",
-          query: { category: label },
-        },
-        {
-          skipEmptyString: true,
-          skipNull: true,
-        }
-      );
-      router.push(url);
-      /* let currentQuery = params ? queryString.parse(params.toString()) : {};
-    const updatedQuery = { ...currentQuery, category: label };
-    const url = queryString.stringifyUrl(
-      {
-        url: "/",
-        query: updatedQuery,
-      },
-      {
-        skipEmptyString: true,
-        skipNull: true,
-      }
-    );
-    router.push(url); */
-    },
-    [] /* [label, params, router] */
-  );
+
+export const Category: React.FC<ICategory> = ({
+  label,
+  Icon,
+  isActive,
+  onClick,
+}) => {
   return (
     <div
-      className={`flex items-center gap-2 p-3 border-b-2  hover:text-slate-800 transition cursor-pointer ${
-        isActive
-          ? "border-b-slate-800 text-slate-800"
-          : "border-transparent text-slate-500"
+      className={`flex items-center gap-2 p-3 hover:text-slate-800 transition cursor-pointer ${
+        isActive ? "text-slate-800" : "text-slate-500"
       }`}
-      onClick={handleClick}
+      onClick={onClick}
     >
       <Icon size={24} />
       <div className="font-medium text-sm">{label}</div>
