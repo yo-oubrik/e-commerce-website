0791cfb5b368ec65a1dff7bc8bb1ab1b1c79e472
Youssef Oubrik <youssefoubrik.edu@gmail.com>
2024-09-10 22:08:25 +0100

    Refactor: extract admin BarGraph component config to separate file for cleaner component

--- app/admin/BarGraph.tsx
+++ app/admin/BarGraph.tsx
@@ -9,7 +9,9 @@ import {
   BarElement,
   scales,
 } from "chart.js";
+import { useMemo } from "react";
 import { Bar } from "react-chartjs-2";
+import { chartOptions, createChartData } from "./config/graphConfig";
 ChartJS.register(
   ArcElement,
   Tooltip,
@@ -22,25 +24,12 @@ interface IBarGraph {
   data: GraphData;
 }
 export const BarGraph: React.FC<IBarGraph> = ({ data }) => {
-  const [labels, amounts] = [Object.keys(data), Object.values(data)];
-  const chartData = {
-    labels,
-    datasets: [
-      {
-        label: "Total amount",
-        data: amounts,
-        backgroundColor: "rgba(75, 192, 192, 0.6)",
-        borderColor: "rgba(75, 192, 192, 1)",
-        borderWidth: 1,
-      },
-    ],
-  };
-  const options = {
-    scales: {
-      y: {
-        beginAtZero: true,
-      },
-    },
-  };
-  return <Bar data={chartData} options={options} />;
+  const labels = Object.keys(data);
+  const amounts = Object.values(data);
+  const title = "Total amount";
+  const chartData = useMemo(
+    () => createChartData(title, labels, amounts),
+    [data]
+  );
+  return <Bar data={chartData} options={chartOptions} />;
 };
