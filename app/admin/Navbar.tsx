92083f73271bd9a2231855ce729b6434433086c7
Youssef Oubrik <youssefoubrik.edu@gmail.com>
2024-09-16 12:30:45 +0100

    Move link to be inside NavItem component

--- app/admin/Navbar.tsx
+++ app/admin/Navbar.tsx
@@ -12,34 +12,30 @@ export const Navbar = () => {
   const pathname = usePathname();
   return (
     <div className="grid xs:grid-cols-2 md:grid-cols-4 max-md:gap-x-3 border-b-[1px] px-2 md:px-3">
-      <Link className="flex items-center md:justify-center" href={"/admin"}>
-        <NavbarItem
-          label={"Summary"}
-          Icon={MdDashboard}
-          isActive={pathname === "/admin"}
-        />
-      </Link>
-      <Link href={"/admin/add-products"}>
-        <NavbarItem
-          label={"Add Products"}
-          Icon={MdLibraryAdd}
-          isActive={pathname === "/admin/add-products"}
-        />
-      </Link>
-      <Link href={"/admin/manage-products"}>
-        <NavbarItem
-          label={"Manage Products"}
-          Icon={MdDns}
-          isActive={pathname === "/admin/manage-products"}
-        />
-      </Link>
-      <Link href={"/admin/manage-orders"}>
-        <NavbarItem
-          label={"Manage Orders"}
-          Icon={MdFormatListBulleted}
-          isActive={pathname === "/admin/manage-orders"}
-        />
-      </Link>
+      <NavbarItem
+        href="/admin"
+        label={"Summary"}
+        Icon={MdDashboard}
+        isActive={pathname === "/admin"}
+      />
+      <NavbarItem
+        href="/admin/add-products"
+        label={"Add Products"}
+        Icon={MdLibraryAdd}
+        isActive={pathname === "/admin/add-products"}
+      />
+      <NavbarItem
+        href="/admin/manage-products"
+        label={"Manage Products"}
+        Icon={MdDns}
+        isActive={pathname === "/admin/manage-products"}
+      />
+      <NavbarItem
+        href="/admin/manage-orders"
+        label={"Manage Orders"}
+        Icon={MdFormatListBulleted}
+        isActive={pathname === "/admin/manage-orders"}
+      />
     </div>
   );
 };
--- app/admin/NavbarItem.tsx
+++ app/admin/NavbarItem.tsx
@@ -1,19 +1,26 @@
+import Link from "next/link";
 import { IconType } from "react-icons";
 
 interface INavbar {
   label: string;
   Icon: IconType;
   isActive: boolean;
+  href: string;
 }
 export const NavbarItem: React.FC<INavbar> = ({ label, Icon, isActive }) => {
   return (
-    <div
-      className={`flex items-center gap-2 p-3 hover:text-slate-800 transition cursor-pointer ${
-        isActive ? "text-slate-800" : "text-slate-500"
-      }`}
+    <Link
+      className="flex items-center md:justify-center"
+      href={"/admin/manage-products"}
     >
-      <Icon size={24} />
-      <div className="font-medium text-sm">{label}</div>
-    </div>
+      <div
+        className={`flex items-center gap-2 p-3 hover:text-slate-800 transition cursor-pointer ${
+          isActive ? "text-slate-800" : "text-slate-500"
+        }`}
+      >
+        <Icon size={24} />
+        <div className="font-medium text-sm">{label}</div>
+      </div>
+    </Link>
   );
 };
